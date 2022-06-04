dbfc25fa81284f16f7028c7aeaac49b03999e89e
ROBIT <robietanam@gmail.com>
2022-06-04 13:40:49 +0700

    front end tambah barang

--- PBO Kasir/StokBarang.Designer.cs
+++ PBO Kasir/StokBarang.Designer.cs
@@ -29,23 +29,197 @@
         private void InitializeComponent()
         {
             this.labe1 = new System.Windows.Forms.Label();
+            this.textBox_NamaBarang = new System.Windows.Forms.TextBox();
+            this.textBox_HargaDasar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_HargaBarang = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_Kode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Stok = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_Keterangan = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox_Kategori = new System.Windows.Forms.ComboBox();
+            this.button_tambahKategori = new System.Windows.Forms.Button();
+            this.button_simpanBarang = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // labe1
             // 
+            this.labe1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.labe1.AutoSize = true;
             this.labe1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.labe1.Location = new System.Drawing.Point(37, 53);
+            this.labe1.Location = new System.Drawing.Point(91, 51);
             this.labe1.Name = "labe1";
             this.labe1.Size = new System.Drawing.Size(95, 17);
             this.labe1.TabIndex = 0;
             this.labe1.Text = "Nama Barang";
             // 
+            // textBox_NamaBarang
+            // 
+            this.textBox_NamaBarang.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox_NamaBarang.Location = new System.Drawing.Point(94, 71);
+            this.textBox_NamaBarang.Name = "textBox_NamaBarang";
+            this.textBox_NamaBarang.Size = new System.Drawing.Size(121, 20);
+            this.textBox_NamaBarang.TabIndex = 1;
+            // 
+            // textBox_HargaDasar
+            // 
+            this.textBox_HargaDasar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox_HargaDasar.Location = new System.Drawing.Point(94, 133);
+            this.textBox_HargaDasar.Name = "textBox_HargaDasar";
+            this.textBox_HargaDasar.Size = new System.Drawing.Size(121, 20);
+            this.textBox_HargaDasar.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(92, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Harga Dasar";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(92, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Kategori (Opsional)";
+            // 
+            // textBox_HargaBarang
+            // 
+            this.textBox_HargaBarang.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox_HargaBarang.Location = new System.Drawing.Point(94, 199);
+            this.textBox_HargaBarang.Name = "textBox_HargaBarang";
+            this.textBox_HargaBarang.Size = new System.Drawing.Size(121, 20);
+            this.textBox_HargaBarang.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(92, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Harga Barang";
+            // 
+            // textBox_Kode
+            // 
+            this.textBox_Kode.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox_Kode.Location = new System.Drawing.Point(314, 71);
+            this.textBox_Kode.Name = "textBox_Kode";
+            this.textBox_Kode.Size = new System.Drawing.Size(121, 20);
+            this.textBox_Kode.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(312, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Kode (Opsional)";
+            // 
+            // textBox_Stok
+            // 
+            this.textBox_Stok.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox_Stok.Location = new System.Drawing.Point(314, 133);
+            this.textBox_Stok.Name = "textBox_Stok";
+            this.textBox_Stok.Size = new System.Drawing.Size(121, 20);
+            this.textBox_Stok.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(312, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Stok";
+            // 
+            // textBox_Keterangan
+            // 
+            this.textBox_Keterangan.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox_Keterangan.Location = new System.Drawing.Point(315, 200);
+            this.textBox_Keterangan.Multiline = true;
+            this.textBox_Keterangan.Name = "textBox_Keterangan";
+            this.textBox_Keterangan.Size = new System.Drawing.Size(200, 65);
+            this.textBox_Keterangan.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.Location = new System.Drawing.Point(311, 179);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Deskripsi";
+            // 
+            // comboBox_Kategori
+            // 
+            this.comboBox_Kategori.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox_Kategori.FormattingEnabled = true;
+            this.comboBox_Kategori.Location = new System.Drawing.Point(95, 268);
+            this.comboBox_Kategori.Name = "comboBox_Kategori";
+            this.comboBox_Kategori.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_Kategori.TabIndex = 16;
+            // 
+            // button_tambahKategori
+            // 
+            this.button_tambahKategori.Location = new System.Drawing.Point(229, 266);
+            this.button_tambahKategori.Name = "button_tambahKategori";
+            this.button_tambahKategori.Size = new System.Drawing.Size(25, 23);
+            this.button_tambahKategori.TabIndex = 17;
+            this.button_tambahKategori.Text = "+";
+            this.button_tambahKategori.UseVisualStyleBackColor = true;
+            // 
+            // button_simpanBarang
+            // 
+            this.button_simpanBarang.Location = new System.Drawing.Point(212, 352);
+            this.button_simpanBarang.Name = "button_simpanBarang";
+            this.button_simpanBarang.Size = new System.Drawing.Size(110, 38);
+            this.button_simpanBarang.TabIndex = 18;
+            this.button_simpanBarang.Text = "Simpan";
+            this.button_simpanBarang.UseVisualStyleBackColor = true;
+            // 
             // StokBarang
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button_simpanBarang);
+            this.Controls.Add(this.button_tambahKategori);
+            this.Controls.Add(this.comboBox_Kategori);
+            this.Controls.Add(this.textBox_Keterangan);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox_Stok);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox_Kode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_HargaBarang);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_HargaDasar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_NamaBarang);
             this.Controls.Add(this.labe1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.Name = "StokBarang";
             this.Size = new System.Drawing.Size(600, 450);
             this.ResumeLayout(false);
@@ -56,5 +230,20 @@
         #endregion
 
         private System.Windows.Forms.Label labe1;
+        private System.Windows.Forms.TextBox textBox_NamaBarang;
+        private System.Windows.Forms.TextBox textBox_HargaDasar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_HargaBarang;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_Kode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Stok;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_Keterangan;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox_Kategori;
+        private System.Windows.Forms.Button button_tambahKategori;
+        private System.Windows.Forms.Button button_simpanBarang;
     }
 }
