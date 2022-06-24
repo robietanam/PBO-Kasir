1f4cb0459a07b4f9f8ccd739c4ffe24d28fce878
ZamzamiIlham <zamzamilham17@gmail.com>
2022-06-24 15:27:27 +0700

    kontol

--- PBO Kasir/Konfirmasi.Designer.cs
+++ PBO Kasir/Konfirmasi.Designer.cs
@@ -26,6 +26,9 @@
         /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
         /// </summary>
+        ///
+
+
         private void InitializeComponent()
         {
             this.button_Edit = new System.Windows.Forms.Button();
